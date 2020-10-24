07cd215c7050ccf5fb562971cf8795577620bb41
ryo-simon_mf <ryosimon1108@gmail.com>
2020-10-25 01:05:38 +0900

    little edit

--- bridge.js
+++ bridge.js
@@ -1,6 +1,8 @@
 const OSC = require('osc-js')
 
 const config = { udpClient: { port: 9876 } }
+    // if you want to change port number, change 'port: ****'
+
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
 
 osc.open() // start a WebSocket server on port 8080
